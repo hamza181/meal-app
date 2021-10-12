ce267a9fd9116f1956e03f943c41d349764db94e
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-10-13 02:07:10 +0500

    Add meal items in different screens dynamically

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,29 +1,51 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { CATEGORIES } from '../data/dummy-data'
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import MealItem from "../components/MealItem";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
 const CategoryMealsScreen = (props) => {
-
-    // get param id from Categories Screen
-    const catId = props.route.params.categoryId
-
-    // find the data from dummy data of id which we get from param
-    // match the id of param to the id of dummy-data
-    const selectedCategory = CATEGORIES.find(value => value.id === catId)
-
+  const renderMealItem = (itemData) => {
     return (
-        <View style={styles.screen}>
-            <Text>{selectedCategory.title}</Text>
-        </View>
-    )
-}
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordabilty={itemData.item.affordabilty}
+        onSelectMeal={() => {}}
+      />
+    );
+  };
 
-export default CategoryMealsScreen
+  // get param id from Categories Screen
+  const catId = props.route.params.categoryId;
+
+  // find the data from dummy data of id which we get from param
+  // match the id of param to the id of dummy-data
+  // const selectedCategory = CATEGORIES.find(value => value.id === catId)
+
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  return (
+    <View style={styles.screen}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: "100%" }}
+      />
+    </View>
+  );
+};
+
+export default CategoryMealsScreen;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
